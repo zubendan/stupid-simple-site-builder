b0aba1eb71a7e947f1512da8acff19b19ce3f2fd
Daniel Zubenko <daniel.zubenko@gmail.com>
2024-11-17 12:26:41 -0800

    started user services

--- src/server/api/service/users.ts
+++ src/server/api/service/users.ts
@@ -1,1 +1,53 @@
-const userService = {};
+import { Prisma, PrismaClient, User } from '@prisma/client';
+import { RoleType } from '~/types/role';
+
+const userService = {
+  listSearchWhere(
+    searchTerms: string[],
+    roles: RoleType[],
+    deleted: boolean,
+    adAccountId: string | null,
+  ): Prisma.UserWhereInput {
+    const containsSearchTerms: Prisma.UserWhereInput[] = [];
+
+    for (const searchTerm of searchTerms) {
+      containsSearchTerms.push({ email: { contains: searchTerm } });
+      containsSearchTerms.push({ firstName: { contains: searchTerm } });
+      containsSearchTerms.push({ lastName: { contains: searchTerm } });
+    }
+
+    return {
+      AND: [
+        {
+          userRoles: {
+            some: {
+              role: {
+                name: {
+                  in: roles,
+                },
+              },
+            },
+          },
+        },
+        ...(deleted ? [{ NOT: { deletedAt: null } }] : [{ deletedAt: null }]),
+        ...(searchTerms.length ? [{ OR: containsSearchTerms }] : []),
+      ],
+    };
+  },
+  async listSearchTotal(
+    db: PrismaClient,
+    searchTerms: string[],
+    roles: RoleType[],
+    deleted: boolean,
+    adAccountId: string | null,
+  ): Promise<number> {
+    const where = userService.listSearchWhere(
+      searchTerms,
+      roles,
+      deleted,
+      adAccountId,
+    );
+
+    return await db.user.count({ where });
+  },
+};
---
+++ src/types/role.ts
@@ -0,0 +1,8 @@
+export enum RoleType {
+  SYSTEM_ADMIN = 'system_admin',
+  SYSTEM_support = 'system_support',
+  ORGANIZATION_ADMIN = 'organization_admin',
+  ORGANIZATION_GUEST = 'organization_guest',
+  ORGANIZATION_USER = 'organization_user',
+  USER = 'user',
+}

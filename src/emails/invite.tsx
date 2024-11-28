be65e247ac7151cdaf30f32198e5251e449620aa
Daniel Zubenko <daniel.zubenko@gmail.com>
2024-11-27 20:22:29 -0800

    email

---
+++ src/emails/invite.tsx
@@ -0,0 +1,115 @@
+import { Organization } from '@prisma/client';
+import {
+  Body,
+  Button,
+  Container,
+  Head,
+  Html,
+  Img,
+  Link,
+  Preview,
+  Section,
+  Text,
+} from '@react-email/components';
+import * as React from 'react';
+import { text } from 'stream/consumers';
+
+interface InviteEmailProps {
+  organizatoinHashid: string;
+  token: string;
+  organizationName: Organization['name'];
+  baseUrl: string;
+}
+
+export const InviteEmail = ({
+  baseUrl,
+  organizationName,
+  organizatoinHashid,
+  token,
+}: InviteEmailProps) => (
+  <Html>
+    <Head />
+    <Preview>This invitation expires after 5 days</Preview>
+    <Body style={main}>
+      <Container style={container}>
+        {/* <Img
+          src={`${BASE_URL}/static/github.png`}
+          width='32'
+          height='32'
+          alt='Github'
+        /> */}
+
+        <Text style={title}>
+          <strong>{organizationName}</strong> has invited you to join their
+          organization on <span style={veras}>VERAS</span>.
+        </Text>
+
+        <Section style={section}>
+          <Link
+            style={button}
+            href={`${baseUrl}/${organizatoinHashid}/invite/${token}`}
+          >
+            Join {organizationName}
+          </Link>
+        </Section>
+      </Container>
+    </Body>
+  </Html>
+);
+
+InviteEmail.PreviewProps = {
+  organizatoinHashid: 'hashid',
+  organizationName: 'Microsoft',
+  baseUrl: 'https://localhost:3000',
+  token: 'token',
+} as InviteEmailProps;
+
+export default InviteEmail;
+
+const veras = {
+  color: '#d66565',
+  background:
+    '-webkit-linear-gradient(45deg, hsl(225 79.2% 81.2%), hsl(221 59.5% 71%), hsl(224 46.7% 61%), hsl(227 39.2% 51%), hsl(228 49.8% 40.6%))',
+  WebkitBackgroundClip: 'text',
+  WebkitTextFillColor: 'transparent',
+  fontWeight: 'bold',
+};
+
+const main = {
+  backgroundColor: 'transparent',
+  color: '#24292e',
+  fontFamily:
+    '-apple-system,BlinkMacSystemFont,"Segoe UI",Helvetica,Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji"',
+};
+
+const container = {
+  maxWidth: '480px',
+  margin: '0 auto',
+  padding: '20px 0 48px',
+};
+
+const title = {
+  fontSize: '24px',
+  lineHeight: 1.25,
+  textAlign: 'center' as const,
+};
+
+const section = {
+  padding: '24px',
+  border: 'solid 1px #dedede',
+  borderRadius: '5px',
+  textAlign: 'center' as const,
+};
+
+const button = {
+  fontSize: '14px',
+  backgroundColor: '#222',
+  background: 'linear-gradient(45deg, #222, #444)',
+  color: '#fff',
+  lineHeight: 1.5,
+  borderRadius: '0.5em',
+  padding: '12px 24px',
+  '&:hover': {
+    background: 'red',
+  },
+};
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,20 @@ export default {
       fontFamily: {
         sans: ['var(--font-geist-sans)', ...fontFamily.sans],
       },
+      colors: {
+        skyfall: {
+          50:"#ecf3ff",
+          100:"#dae2f8",
+          200:"#b5c3e7",
+          300:"#8da1d7",
+          400:"#6b85c9",
+          500:"#5673c1",
+          600:"#4a69bf",
+          700:"#3a59a9",
+          800:"#314f98",
+          900:"#244488",
+       }
+     }
     },
   },
   plugins: [],

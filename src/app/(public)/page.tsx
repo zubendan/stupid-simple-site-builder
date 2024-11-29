a1978bc0457deb16840971a7cd1de77780e1afed
Daniel Zubenko <daniel.zubenko@gmail.com>
2024-11-29 12:50:42 -0800

    modal design bit

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -2,6 +2,7 @@ import Link from 'next/link';
 
 import { auth } from '~/server/auth';
 import { HydrateClient, api } from '~/trpc/server';
+import { routes } from '~/utils/routes';
 
 export default async function Home() {
   const session = await auth();
@@ -43,7 +44,7 @@ export default async function Home() {
                 {session && <span>Logged in as {session.user?.name}</span>}
               </p>
               <Link
-                href={session ? '/api/auth/signout' : '/api/auth/signin'}
+                href={session ? routes.SIGN_OUT : routes.SIGN_IN}
                 className='rounded-full bg-white/10 px-10 py-3 font-semibold no-underline transition hover:bg-white/20'
               >
                 {session ? 'Sign out' : 'Sign in'}
--- src/app/(public)/sign-in/page.tsx
+++ src/app/(public)/sign-in/page.tsx
@@ -1,20 +1,34 @@
+'use client';
 import { getProviders, signIn } from 'next-auth/react';
+import { ClientSafeProvider } from 'node_modules/next-auth/lib/client';
+import { useEffect, useState } from 'react';
 
-export default async function Page() {
-  const providers = await getProviders();
+export default function Page() {
+  const [providers, setProviders] = useState<ClientSafeProvider[]>([]);
+  useEffect(() => {
+    const func = async () => {
+      const providers = await getProviders();
+      if (providers !== null) {
+        setProviders(Object.values(providers));
+      }
+    };
+    func();
+  }, []);
 
   return (
     <div className='h-lvh w-lvw [background:linear-gradient(45deg,hsl(225_79.2%_81.2%),hsl(221_59.5%_71%),hsl(224_46.7%_61%),hsl(227_39.2%_51%),hsl(228_49.8%_40.6%))] flex justify-center items-center'>
-      <div>
-        <h1>Some Sign In Text</h1>
-        <div>
-          // a form
-          {providers &&
-            Object.values(providers).map((provider) => (
-              <button onClick={() => signIn(provider.id)}>
-                Sign in with {provider.name}
-              </button>
-            ))}
+      <div className='w-[600px] text-center'>
+        <h1 className='text-5xl font-semibold bg-gradient-to-tr from-neutral-950 via-neutral-800 to-neutral-600 bg-clip-text text-transparent pb-6'>
+          Some Sign In Text
+        </h1>
+        <div className='bg-white p-6 rounded-md'>
+          {/* // a form */}
+
+          {providers.map((provider) => (
+            <button key={provider.id} onClick={() => signIn(provider.id)}>
+              Sign in with {provider.name}
+            </button>
+          ))}
         </div>
       </div>
     </div>

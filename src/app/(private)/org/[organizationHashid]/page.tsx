348e4dbf70319f6b50c76070db86f85e0cae4d42
Daniel Zubenko <daniel.zubenko@gmail.com>
2025-01-23 19:06:50 -0800

    routing change

---
+++ src/app/(private)/org/[organizationHashid]/page.tsx
@@ -0,0 +1,9 @@
+import { redirect } from 'next/navigation';
+import { routes } from '~/utils/routes';
+
+export default async function Page({
+  params,
+}: { params: Promise<{ organizationHashid: string }> }) {
+  const { organizationHashid } = await params;
+  redirect(routes.TEMPLATES(organizationHashid));
+}
